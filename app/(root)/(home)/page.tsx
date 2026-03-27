377d2974ffcc0b7ca68011eea6c12dcb7f0263af
Hung <hungnguyen25092004@gmail.com>
2026-03-27 19:45:09 +0700

    update

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,22 +1,53 @@
+'use client';
+
 import MeetingTypeList from '@/components/MeetingTypeList';
+import { useGetCalls } from '@/hooks/useGetCalls';
 
 const Home = () => {
   const now = new Date();
 
+  const { upcomingCalls } = useGetCalls();
+  const nextMeeting = upcomingCalls?.[0];
 
-  const time = now.toLocaleTimeString('vi-VN', { hour: '2-digit', minute: '2-digit' ,hour12: true });
-  const date = now.toLocaleDateString('vi-VN', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+const meetingTime = nextMeeting?.state?.startsAt
+  ? new Date(nextMeeting.state.startsAt).toLocaleString('vi-VN', {
+      hour: '2-digit',
+      minute: '2-digit',
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    })
+  : null;
+
+  const time = now.toLocaleTimeString('vi-VN', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true,
+  });
+
+  const date = now.toLocaleDateString('vi-VN', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+
           <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting at: 12:30 PM
+            {meetingTime
+              ? `Cuộc họp tiếp theo lúc: ${meetingTime}`
+              : 'Không có cuộc họp sắp tới'}
           </h2>
+
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
-            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">
+              {date}
+            </p>
           </div>
         </div>
       </div>
